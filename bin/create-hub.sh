291389a40f24282ffa5dd34948fbb2688a3996fb
Sami Laine <sami.v.laine@aalto.fi>
2023-04-25 13:41:15 +0300

    jupyter-test -> jupyter-jh-v4-test

--- bin/create-hub.sh
+++ bin/create-hub.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-NAMESPACE=${1:-jupyter-test}
+NAMESPACE=${1:-jupyter-jh-v4-test}
 # shellcheck source-path=bin
 source "$SCRIPTPATH/_check_namespace.sh"
 

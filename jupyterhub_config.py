20a5e31f19d0dbea05fdb767068edd9cc7f6d295
Sami Laine <sami.v.laine@aalto.fi>
2023-07-17 11:51:28 +0300

    Revert "Temporarily disable stats+spawn_test"
    This reverts commit 961b47c6c235ad9bf1e028450b5366be252da8ab.

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -1262,15 +1262,17 @@ c.JupyterHub.services = [
         ],
     },
     # Service to show stats.
-    # {
-    #     "name": "stats",
-    #     "admin": True,
-    #     "url": "http://%s:36541" % os.environ["JUPYTERHUB_SVC_SERVICE_HOST"],
-    #     "command": [
-    #         "python3",
-    #         "/srv/jupyterhub/hub_status_service.py"
-    #         if os.path.exists("/srv/jupyterhub/hub_status_service.py")
-    #         else "/hub_status_service.py",
-    #     ],
-    # },
+    {
+        "name": "stats",
+        "admin": True,
+        "url": "http://%s:36541" % os.environ["JUPYTERHUB_SVC_SERVICE_HOST"],
+        "command": [
+            "python3",
+            (
+                "/srv/jupyterhub/hub_status_service.py"
+                if os.path.exists("/srv/jupyterhub/hub_status_service.py")
+                else "/hub_status_service.py"
+            ),
+        ],
+    },
 ]

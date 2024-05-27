2db017f944e9f554e757417eb8196db911fab6f6
Sami Laine <sami.v.laine@aalto.fi>
2024-05-27 18:04:20 +0300

    config: Add auth_state_hook

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -962,9 +962,17 @@ def clear_state(spawner: KubeSpawner):
         if hasattr(spawner, name):
             delattr(spawner, name)
 
+def auth_state_hook(spawner: KubeSpawner, auth_state: dict):
+    if spawner.user.name == "laines5":
+        spawner.log.info("auth_state_hook: %s %s", spawner.user.name, auth_state)
+        c.JupyterHub.template_vars = {
+            "announcement_hook": "This is an announcement about hooks, specifically for laines5",
+        }
+
 
 c.KubeSpawner.pre_spawn_hook = pre_spawn_hook
 c.KubeSpawner.post_stop_hook = post_stop_hook
+c.KubeSpawner.auth_state_hook = auth_state_hook
 KubeSpawner.get_state = get_state
 KubeSpawner.load_state = load_state
 KubeSpawner.clear_state = clear_state
